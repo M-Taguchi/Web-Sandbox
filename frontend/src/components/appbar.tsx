345d24d1e6583cf6e808556fac7a5d4e8e14f4f9
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2022-01-08 22:38:12 +0900

    レイアウトを少しリッチに

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -1,4 +1,4 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
 import {
   Box,
   Input,
@@ -10,24 +10,32 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
+  Link,
+  Stack,
+  Flex,
+  InputGroup,
+  InputLeftElement,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
 
-type AppbarProps = {
-};
+type AppbarProps = {};
 
 const Appbar: React.FC = () => {
-  const [ isOpen, setIsOpen ] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Box p={4}>
-      <Button colorScheme="teal" onClick={() => {
+      <Button
+        colorScheme="teal"
+        onClick={() => {
           setIsOpen(true);
-        }}>
-        <HamburgerIcon/>
+        }}
+      >
+        <HamburgerIcon />
       </Button>
       {/* TODO: アンマウント時のアニメーション追加 */}
-      {isOpen && 
+      {isOpen && (
         <Drawer
           isOpen={isOpen}
           placement="left"
@@ -37,21 +45,72 @@ const Appbar: React.FC = () => {
         >
           <DrawerOverlay />
           <DrawerContent>
-            <DrawerCloseButton onClick={() => {
-            setIsOpen(false);
-          }}/>
-            <DrawerHeader>Create your account</DrawerHeader>
+            <DrawerHeader>
+              メニュー
+              <DrawerCloseButton
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              />
+            </DrawerHeader>
 
             <DrawerBody>
-              <Input placeholder="Type here..." />
+              <Flex
+                direction={"column"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                textAlign={"center"}
+                gap={1}
+              >
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    <SearchIcon />
+                  </InputLeftElement>
+                  <Input placeholder="検索" mb={2} />
+                </InputGroup>
+                <Link
+                  display={"Flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  w={"100%"}
+                  h={12}
+                  _hover={{ bg: "gray.100" }}
+                  as={ReactRouterLink}
+                  to="/test"
+                >
+                  ホーム
+                </Link>
+                <Link
+                  display={"Flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  w={"100%"}
+                  h={12}
+                  _hover={{ bg: "gray.100" }}
+                  as={ReactRouterLink}
+                  to="/test"
+                >
+                  どっか
+                </Link>
+                <Link
+                  display={"Flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  w={"100%"}
+                  h={12}
+                  _hover={{ bg: "gray.100" }}
+                  as={ReactRouterLink}
+                  to="/test"
+                >
+                  異次元
+                </Link>
+              </Flex>
             </DrawerBody>
 
-            <DrawerFooter>
-              <Button colorScheme="blue">Save</Button>
-            </DrawerFooter>
+            <DrawerFooter></DrawerFooter>
           </DrawerContent>
         </Drawer>
-      }
+      )}
     </Box>
   );
 };
