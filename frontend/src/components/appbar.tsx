d258aed802323a4f44a0bab7a4c4ce9140b5251c
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2022-01-08 23:09:44 +0900

    アイコン追加

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -15,9 +15,12 @@ import {
   Flex,
   InputGroup,
   InputLeftElement,
+  Text,
+  Icon,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
+import { MdHome, MdSearch } from "react-icons/md";
 
 type AppbarProps = {};
 
@@ -25,7 +28,7 @@ const Appbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Box p={4}>
+    <Box p={4} bg="teal.300">
       <Button
         colorScheme="teal"
         onClick={() => {
@@ -64,45 +67,54 @@ const Appbar: React.FC = () => {
               >
                 <InputGroup>
                   <InputLeftElement pointerEvents="none">
-                    <SearchIcon />
+                    <Icon as={MdSearch} w={6} h={6} />
                   </InputLeftElement>
                   <Input placeholder="検索" mb={2} />
                 </InputGroup>
                 <Link
                   display={"Flex"}
                   alignItems={"center"}
-                  justifyContent={"center"}
                   w={"100%"}
+                  pl={1}
                   h={12}
                   _hover={{ bg: "gray.100" }}
                   as={ReactRouterLink}
                   to="/test"
                 >
-                  ホーム
+                  <Flex gap={3}>
+                    <Icon as={MdHome} w={6} h={6} />
+                    <Text>ホーム</Text>
+                  </Flex>
                 </Link>
                 <Link
                   display={"Flex"}
                   alignItems={"center"}
-                  justifyContent={"center"}
                   w={"100%"}
                   h={12}
+                  pl={1}
                   _hover={{ bg: "gray.100" }}
                   as={ReactRouterLink}
                   to="/test"
                 >
-                  どっか
+                  <Flex gap={3}>
+                    <Icon as={MdHome} w={6} h={6} />
+                    <Text>どっか</Text>
+                  </Flex>
                 </Link>
                 <Link
                   display={"Flex"}
                   alignItems={"center"}
-                  justifyContent={"center"}
                   w={"100%"}
                   h={12}
+                  pl={1}
                   _hover={{ bg: "gray.100" }}
                   as={ReactRouterLink}
                   to="/test"
                 >
-                  異次元
+                  <Flex gap={3}>
+                    <Icon as={MdHome} w={6} h={6} />
+                    <Text>異次元</Text>
+                  </Flex>
                 </Link>
               </Flex>
             </DrawerBody>
