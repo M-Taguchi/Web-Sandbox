cda9075602f785a0e8d02dfc0315657b058346e3
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2022-01-08 17:32:28 +0900

    overlay周り修正

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -12,39 +12,51 @@ import {
   DrawerHeader,
   DrawerOverlay,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 type AppbarProps = {
 };
 
 const Appbar: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [ isOpen, setIsOpen ] = useState(false);
+  const { onOpen, onClose } = useDisclosure();
 
   return (
     <Box>
-      <Button colorScheme="teal" onClick={onOpen}>
+      <Button colorScheme="teal" onClick={() => {
+          onOpen();
+          setIsOpen(true);
+        }}>
         <HamburgerIcon/>
       </Button>
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        variant={"alwaysOpen"}
-      >
-        {/* 入れるとフォーカスが上手く働かない */}
-        {/* <DrawerOverlay/> */}
-        <DrawerContent>
-          <DrawerCloseButton onClick={onClose}/>
-          <DrawerHeader>Create your account</DrawerHeader>
+      {/* TODO: アンマウント時のアニメーション追加 */}
+      {isOpen && 
+        <Drawer
+          isOpen={isOpen}
+          placement="left"
+          onClose={() => {
+            onClose();
+            setIsOpen(false);
+          }}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton onClick={() => {
+            onClose();
+            setIsOpen(false);
+          }}/>
+            <DrawerHeader>Create your account</DrawerHeader>
 
-          <DrawerBody>
-            <Input placeholder="Type here..." />
-          </DrawerBody>
+            <DrawerBody>
+              <Input placeholder="Type here..." />
+            </DrawerBody>
 
-          <DrawerFooter>
-            <Button colorScheme="blue">Save</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+            <DrawerFooter>
+              <Button colorScheme="blue">Save</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      }
     </Box>
   );
 };
